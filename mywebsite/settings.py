40201916ddc0d3a6f36ea9e92201a6d49ecb5d27
Thiago Laet <thi4golaet@gmail.com>
2022-10-14 21:28:33 -0300

    aaaaaaa

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages'
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
 
     'chat'
 ]
